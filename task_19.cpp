b0d75eddae82f68c00b3d4ea1fc4fc5c692730b1
vlad <ecscalibur_10@mail.ru>
2024-04-29 13:19:43 +0300

    tasks_2

---
+++ task_19.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+class String{
+public:
+    char *str;
+    int size;
+
+    explicit String(string str){
+        this->str = new char [str.size()];
+        this->size = (int)str.size();
+        for (int i = 0; i < this->size; ++i) this->str[i] = str[i];
+    }
+
+    void reverse(int i, int j){
+        i = i - 1;
+        j = j - 1;
+        if (i > j || i < 0 || j >= this->size) throw range_error("Incorrect indices");
+        int k = 0;
+        char change;
+        while (i + k < j - k){
+            change = this->str[i + k];
+            this->str[i + k] = this->str[j - k];
+            this->str[j - k] = change;
+            k++;
+        }
+    }
+
+    friend ostream& operator << (ostream& out, String& str);
+
+    ~String(){
+        delete[] this->str;
+    }
+};
+
+ostream &operator<<(ostream &out, String& str) {
+    for (int i = 0; i < str.size; ++i) out << str.str[i];
+    return out;
+}
+
+int main(){
+    int i, j;
+    string str;
+    cin >> i >> j >> str;
+    String s = String(str);
+    s.reverse(i, j);
+    cout << s;
+    return 0;
+}
